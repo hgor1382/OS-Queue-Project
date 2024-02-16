9a6ec9d044a6a70468ef199d1342eab93302a759
hgor1382 <hgor1382@gmail.com>
2024-02-16 11:34:34 +0330

    difine necessary variables for MassageQueue class

--- Queue.java
+++ Queue.java
@@ -2,5 +2,13 @@ import java.util.*;
 import java.io.*;
 
 Class MassageQueue{
-    
+    private final Map<String, LinkedList<String>> queues;
+    private final int capacity;
+    private final Map<String, Semaphore> semProducers;
+    private final Map<String, Semaphore> semConsumers;
+    private final Map<String, AtomicInteger> numMessages;
+    private final Map<String, Integer> totalVolume;
+    private final Semaphore mutex;
+    private final Semaphore r_mutex;
+    private final int maxVolume;
 }
