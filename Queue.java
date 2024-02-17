3f2ac8dc51bf74644dcee0cb461785e4a088825d
hgor1382 <hgor1382@gmail.com>
2024-02-17 12:57:11 +0330

    tests created(threads)

--- Queue.java
+++ Queue.java
@@ -209,3 +209,217 @@ class Stats {
     int volume;
     long time;
 }
+
+public class project {
+    public static void main(String[] args) throws InterruptedException {     
+        MessageQueue messageQueue = new MessageQueue(2,10000);
+        messageQueue.createTopic("topic 1");
+        messageQueue.createTopic("topic 2");
+
+        long startTime = System.currentTimeMillis();
+
+        Thread producer1 = new Thread(() -> {
+            try {
+                messageQueue.msg_send("topic 1","Message 1 from Producer 1", startTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread producer2 = new Thread(() -> {
+            try {
+                messageQueue.msg_send("topic 1","Message 2 from Producer 2", startTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        Thread producer3 = new Thread(() -> {
+            try {
+                messageQueue.msg_send("topic 1","Message 3 from Producer 3", startTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        Thread producer4 = new Thread(() -> {
+            try {
+                messageQueue.msg_send("topic 1","Message 4 from Producer 4", startTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread consumer1 = new Thread(() -> {
+            try {
+                String message = messageQueue.msg_get("topic 1", startTime);
+                System.out.println("Consumer 1 received: " + message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread consumer2 = new Thread(() -> {
+            try {
+                String message = messageQueue.msg_get("topic 1", startTime);
+                System.out.println("Consumer 2 received: " + message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        Thread consumer5 = new Thread(() -> {
+            try {
+                String message = messageQueue.msg_get("topic 1", startTime);
+                System.out.println("Consumer 5 received: " + message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        Thread consumer3 = new Thread(() -> {
+            String message = messageQueue.nb_msg_get("topic 1", startTime);
+            System.out.println("Consumer 3 received: " + message);
+        });
+
+        Thread consumer4 = new Thread(() -> {
+            String message = messageQueue.nb_msg_get("topic 1", startTime);
+            System.out.println("Consumer 4 received: " + message);
+        });
+
+        Thread stats1 = new Thread(() -> {
+            try {
+                Stats s = messageQueue.stats("topic 1", startTime);
+                System.out.println("num messages: " + s.numMessages + " total length: " + s.totalLength + " mem: " + s.memoryUsage + " at time: " + s.time);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Thread stats2 = new Thread(() -> {
+            try {
+                Stats s = messageQueue.stats("topic 1", startTime);
+                System.out.println("num messages: " + s.numMessages + " total length: " + s.totalLength + " mem: " + s.memoryUsage + " at time: " + s.time);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Thread stats3 = new Thread(() -> {
+            try {
+                Stats s = messageQueue.stats("topic 1", startTime);
+                System.out.println("num messages: " + s.numMessages + " total length: " + s.totalLength + " mem: " + s.memoryUsage + " at time: " + s.time);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Thread stats4 = new Thread(() -> {
+            try {
+                Stats s = messageQueue.stats("topic 1", startTime);
+                System.out.println("num messages: " + s.numMessages + " total length: " + s.totalLength + " mem: " + s.memoryUsage + " at time: " + s.time);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        
+
+        //Test 1:
+
+        // consumer1.start();
+        // System.out.println(messageQueue.stats("topic 1" , startTime).queue);
+        // Thread.sleep(1000);
+        // producer1.start();
+
+        //Test 2: some producers, one consumer
+
+        // producer1.start();
+        // producer2.start();
+        // Thread.sleep(100);
+        // consumer1.start();
+
+
+        // Test 3: some consumers, one producer
+
+        // producer3.start();
+        // Thread.sleep(100);
+        // consumer2.start();
+        // consumer1.start();
+
+
+        //Test 4:non-blocking
+        
+        // consumer3.start();
+        // Thread.sleep(100);
+        // producer1.start();
+        // producer2.start();
+        // Thread.sleep(100);
+        // consumer4.start();
+        // Thread.sleep(100);
+        // consumer1.start();
+
+
+
+        //Test 5: capacity 2
+
+        // producer1.start();
+        // producer2.start();
+        // producer3.start();
+        // producer4.start();
+        // stats1.start();
+        // Thread.sleep(100);
+        // consumer1.start();
+        // Thread.sleep(100);
+        // consumer2.start();
+        // stats2.start();
+
+
+        // Test 6:
+        
+        // consumer1.start();
+        // consumer2.start();
+        // consumer5.start();
+        
+        // producer1.start();
+        // stats1.start();
+        // producer2.start();
+        // producer3.start();
+        // stats2.start();
+        // producer4.start();
+       
+
+
+        //Test 7: saving capacity 3
+
+        // producer1.start();
+        // producer2.start();
+        // producer3.start();
+
+        // Thread.sleep(100);
+        // messageQueue.saveQueues("save.dat");
+
+        // MessageQueue ms = messageQueue.readQueues("save.dat");
+        
+        // Thread producer5 = new Thread(() -> {
+        //     try {
+        //         ms.msg_send("topic 1","Message 5 from Producer 5", startTime);
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+        // });
+
+        // Thread consumer6 = new Thread(() -> {
+        //     String message = ms.nb_msg_get("topic 1", startTime);
+        //     System.out.println("Consumer 6 received: " + message);
+        // });
+
+        // Thread consumer7 = new Thread(() -> {
+        //     String message = ms.nb_msg_get("topic 1", startTime);
+        //     System.out.println("Consumer 7 received: " + message);
+        // });
+        // System.out.println(ms.stats("topic 1",startTime).queue);
+
+        // producer5.start();
+        // Thread.sleep(500);
+        
+        // consumer6.start();
+        // consumer7.start();
+    }
+}
